903c3f6df7d5cf4e86310de52caf293991a4075e
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2024-08-06 14:31:56 +0530

    Add files via upload

---
+++ percentage_of_subject.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class percentage_of_subject {
+    public static void main(String[] args) {
+        // code for caculating percenatge
+        double x = 70;  //hindi marks
+        double y = 78;  //math marks
+        double z = 69;  //english marks
+        double a = 98;  //science marks
+        double b = 89; //art marks
+        double percentage = (x + y + z + a + b)/5;
+        System.out.println(percentage);
+    }
+}
